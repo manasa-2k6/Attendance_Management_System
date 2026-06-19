4e6889d1225c9df9c07b740a655a53825452e8c1
Manasa Kanchibhatla <manasakanchibatla@gmail.com>
2026-06-19 14:16:09 +0530

    basic javascript practice

---
+++ Fronted/javaScript-practice.js
@@ -0,0 +1,62 @@
+// console.log("Welcome students")
+// var a=10
+// var b="10"
+// console.log(a===b)
+// console.log(name)
+// var name='kumar'
+// console.log(name)
+// name='arun'
+// console.log(name)
+// function greet(){
+//     var a='Welcome'
+//     console.log(a)
+// }
+// console.log(a)  
+
+// if(true){
+//     var c = 100
+//     console.log(c)
+// }
+// console.log(c)
+// if(true){
+//      let c = 100
+//      console.log(c)
+//  }
+//  console.log(c)
+// const a=10
+// const a=10
+// console.log(a)
+// var a=10
+// var b="25"
+// console.log(a+b)
+// let a=10 
+// a+=20
+// console.log(a++)
+// const a=10
+// console.log(typeof a)
+// const b="10"
+// console.log(typeof b)
+// console.log(a==b)
+// const c=70 
+// const d=c>50?"YES":"NO"
+// console.log(checking)
+// const age=12
+// if(age>=18){
+//     console.log("Eligible for voting")
+// }
+// else{
+//     console.log("Not Eligible for voting")
+// }
+//greater among three numbers
+const a=10
+const b=20
+const c=15
+if (a>b && a>c){
+    console.log("a is grater")
+}
+else if(b>a && b>c){
+    console.log("b is greater")
+}
+else{
+    console.log("c is greater")
+}
